7085e1e017196be7dc74a0512d2210772ee7475b
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-07-20 14:52:08 +0545

    add

--- Client/src/pages/login.tsx
+++ Client/src/pages/login.tsx
@@ -1,8 +1,40 @@
+import "./auth/login.css";
 const Login = () => {
   return (
-    <div>
-      <h1>This is Login Page</h1>
-    </div>
+    <main>
+      <h1>
+        Login
+        {/*form*/}
+      </h1>
+      <div className="wrapper">
+        {/*email*/}
+
+        <div className="input-wrapper">
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            id="email"
+            placeholder="shopkart@gmail.com"
+            type="email"
+          />
+        </div>
+        {/*password*/}
+        <div className="input-wrapper">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+            type="password"
+          />
+        </div>
+        {/*Login*/}
+        <br />
+        <div className="input-wrapper">
+          <button>Login</button>
+        </div>
+      </div>
+    </main>
   );
 };
 
